867a949c2556d5040060637d353dc4246b172af7
ae711e <ae711e@gmail.com>
2020-03-26 13:52:52 +0300

    ред.

--- src/srv/Datr.java
+++ src/srv/Datr.java
@@ -5,86 +5,84 @@
  */
 
 /*
-   Прочитать дату чтения сообщения
+   Прочитать даты чтения сообщений
  */
 
 package srv;
 
 import ae.R;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 
+/**
+ * получить даты чтения сообщений
+ */
 public class Datr extends ServerData {
   private final static String sKey = "datr";  // ключ метки
-  private final static String sKey2 = "datrs";  // ключ метки для списка сообщений
 
   /**
-   * получить дату чтения сообщения для текущего пользователя
-   * @param imStr строка с номером сообщения
-   * @return дата чтения сообщения
-   */
-  public String get(String imStr)
-  {
-    try {
-      int im = Integer.parseInt(imStr);
-      return get(im);
-    } catch (Exception e) {
-      System.err.println("?-error-Datr.get() ошибка в номере сообщения: " +  imStr + ". " + e.getMessage());
-    }
-    return null;
-  }
-
-  /**
-   * получить дату чтения сообщение для текущего пользователя
-   * @param im  номер сообщения
-   * @return дата чтения сообщения
-   */
-  public String get(int im)
-  {
-    final String uTo = R.getUsr(); // пользователь (получатель)
-    final String pwd = R.getUsrPwd(uTo); // пароль пользователя получателя
-    Map<String, String> args = prepareArgs(
-        "im", String.valueOf(im),
-        "pwd", pwd
-    );
-    String[] ars;
-    ars = super.postStr(sKey, args);
-    if(ars != null && ars.length > 0) {
-      // расшифруем сообщение
-      String datr = ars[0];  // дата yyyy-mm-dd hh:mm:ss
-      if(datr != null && datr.length() > 9) {
-        return datr;
-      }
-    }
-    return null;
-  }
-
-  /**
-   * выдать список дат чтения сообщений
-   * @param imsStr список масивов строк, где первый элемент - номер сообщения
+   * выдать список дат чтения сообщений (дата будет в формате SQL YYYY-MM-DD hh:mm:ss)
+   * @param imStr  список масивов строк, где первый элемент - номер сообщения
    *               [ ["номер1"], ["номер2"] ]
-   * @return массив [ ["номер1","дата_чтения2"], ["номер2","дата_чтения2"] ]
+   * @return массив [ ["номер1","дата_чтения1"], ["номер2","дата_чтения2"] ]
    */
-  public List<String[]> get(List<String[]> imsStr)
+  public List<String[]> get(List<String[]> imStr)
   {
-    if(imsStr == null || imsStr.size() < 1)
+    if(imStr == null || imStr.size() < 1)
       return null;
     //
     final String uTo = R.getUsr();        // пользователь (получатель)
     final String pwd = R.getUsrPwd(uTo);  // пароль пользователя получателя
     // https://docs.oracle.com/javase/8/docs/api/java/util/StringJoiner.html
     StringJoiner  sbuf = new StringJoiner(",", "[", "]");
-    for(String[] r: imsStr) { sbuf.add(r[0]); }
+    for(String[] r: imStr) { sbuf.add(r[0]); }
     //
     Map<String, String> args = prepareArgs(
         "ims", sbuf.toString(),
         "pwd", pwd
     );
     //
-    return postList(sKey2, args);
+    return postList(sKey, args);
   }
 
+//  /**
+//   * получить дату чтения сообщения для текущего пользователя
+//   * @param imStr строка с номером сообщения
+//   * @return дата чтения сообщения
+//   */
+//  public String get(String imStr)
+//  {
+//    try {
+//      int im = Integer.parseInt(imStr);
+//      return get(im);
+//    } catch (Exception e) {
+//      System.err.println("?-error-Datr.get() ошибка в номере сообщения: " +  imStr + ". " + e.getMessage());
+//    }
+//    return null;
+//  }
+//
+//  /**
+//   * получить дату чтения сообщение для текущего пользователя
+//   * @param im  номер сообщения
+//   * @return дата чтения сообщения
+//   */
+//  public String get(int im)
+//  {
+//    ArrayList<String[]> ari = new ArrayList<>();
+//    String[] r = new String[1];
+//    r[0] = String.valueOf(im);
+//    ari.add(r);
+//    List<String[]> ol = get(ari);
+//    if(ol != null) {
+//      r = ol.get(0);
+//      if(r != null && r.length > 1) {
+//        return r[1];
+//      }
+//    }
+//    return null;
+//  }
 
 } // end of class
